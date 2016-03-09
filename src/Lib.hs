ae664c13ddee42e22ab1ca5796b1d93d5147b7a4
meditans <meditans@gmail.com>
2016-03-09 20:26:04 +0100

    Updated commentary

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,11 @@
 -- * A Variety of Quantifiers
 
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE PolyKinds      #-}
-{-# LANGUAGE TypeFamilies   #-}
-{-# LANGUAGE TypeOperators  #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Lib where
 
@@ -43,7 +43,9 @@ vchop :: Natty m -> Vec (m :+ n) x -> (Vec m x, Vec n x)
 vchop Zy     xs        = (V0, xs)
 vchop (Sy n) (x :> xs) = let (ys, zs) = vchop n xs in (x :> ys, zs)
 
--- Now for a further disturbance:
+-- ** Expliciting static data
+
+-- We can't do this:
 
 -- #+BEGIN_EXAMPLE
 -- vtake :: Natty m -> Vec (m :+ n) x -> Vec m x
@@ -51,10 +53,32 @@ vchop (Sy n) (x :> xs) = let (ys, zs) = vchop n xs in (x :> ys, zs)
 -- vtake (Sy n) (x :> xs) = let ys = vtake n xs in x :> ys
 -- #+END_EXAMPLE
 
--- Please detail why this code is not compilable.
+-- Please detail why this code is not compilable: it's not clear how to instantiate
+-- n in the recursive call. This is because we cannot deduce ~n~ from ~m :+ n~ and
+-- ~m~, because that is essentially the injectivity of ~:+~, which is not derivable
+-- from ghc, so in ~vtake n xs~ what should be the type of xs?
 
--- Our problem is then that we have some static data, which has to be made
--- explicit. So we just define:
+-- So we show that the problem is essentially a problem with the signature itself,
+-- and not the particular implementation:
+-- #+BEGIN_EXAMPLE
+-- vtake :: Natty m -> Vec (m :+ n) x -> Vec m x
+-- vtake = undefined
+-- #+END_EXAMPLE
+
+-- This is because the type signature for vtake2 would be:
+-- #+BEGIN_EXAMPLE
+-- vtake :: forall (m :: Nat) (n :: Nat) x. Natty m -> Vec (m :+ n) x -> Vec m x
+-- #+END_EXAMPLE
+
+-- And so we can see that the choice of n is ambigous (we cannot determine it from
+-- a function call). The suggestion in the error is to enable
+-- =AllowAmbiguousTypes=, to defer the check to the use sites. If we try that, we
+-- discover that it is indeed possible to get the signature to compile, but there
+-- is no way to write the actual code. The problem is fundamentally insurmountable
+-- because we don't know how to instantiate ~n~ in the following invocation.
+
+-- Our problem is then that we have *some static data, which has to be made
+-- explicit*. So we just define:
 
 data Proxy :: κ -> * where
   Proxy :: Proxy i
@@ -62,6 +86,9 @@ data Proxy :: κ -> * where
 vtake :: Natty m -> Proxy n -> Vec (m :+ n) x -> Vec m x
 vtake Zy     n xs        = V0
 vtake (Sy m) n (x :> xs) = x :> vtake m n xs
+
+-- The proxy is useful only at the type level, and lets us tag the particular n
+-- that we want.
 
 -- Another example of implicit quantification used statically to compute a type
 -- but erased at run time:
@@ -73,4 +100,3 @@ type instance Arity (S n) x = x -> Arity n x
 varity :: Arity n x -> Vec n x -> x
 varity x V0 = x
 varity fxAnx (x :> xs) = varity (fxAnx x) xs
-
